610ca13c6d7717c39199158e118bf849448f8f3f
Travis Wagner <travis@travisaw.com>
2021-02-10 20:54:34 -0500

    add footer

---
+++ wp-content/themes/sherwood/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Theme Footer
+ *
+ * @package sherwood
+ */
+
+?>
+	<nav id="scrollToTop" class="scroll-to-top position-fixed end-0 bottom-0 me-3 mb-3 z-1">
+		<a href="#" class="inner d-block position-relative border border-dark p-3 hover-no-pseudo hover-muted">
+			<i class="far fa-fw fa-arrow-up"></i>
+		</a>
+	</nav>
+</main><!-- #main -->
+<?php wp_footer(); ?>
+</body>
+</html>
