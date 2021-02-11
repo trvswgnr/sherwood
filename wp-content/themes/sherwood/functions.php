b27a9af51accf4eb179ab02b8260c00a93fcb692
Travis Wagner <travis@travisaw.com>
2021-02-10 21:16:55 -0500

    update

--- wp-content/themes/sherwood/functions.php
+++ wp-content/themes/sherwood/functions.php
@@ -9,7 +9,7 @@ require_once 'classes/class-script-loader.php';
 require_once 'classes/class-theme.php';
 require_once 'classes/class-sherwood.php';
 
-$emma = new Sherwood();
+$sherwood = new Sherwood();
 
 function get_cdn_url($filename) {
 	$base = 'https://cdn.jsdelivr.net/gh/trvswgnr/cdn@latest/';
