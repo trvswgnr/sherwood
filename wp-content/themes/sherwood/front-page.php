0c01a5331f8953ff2c747477b2ae6290c6c7d41f
Travis Wagner <travis@travisaw.com>
2021-02-19 00:40:43 -0500

    add front page

---
+++ wp-content/themes/sherwood/front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the home page
+ *
+ * @package sherwood
+ */
+
+get_header();
+?>
+<article id="content" class="content content-home" role="content">
+	<section class="hero section">
+		<div class="container container-md">
+			<h1>A Place to Gather</h1>
+			<p>Beautiful Sherwood Gardens is situated on 10 acres and is eagerly awaiting your next wedding, gathering or celebration. The 1903 historic home is fully equipped with everything you would need for your stay including 5 bedrooms, 3 bathrooms, and 2 glassed in sun porches. The Event Barn has a main floor & upstairs for celebrating & can accommodate up to 150 guests. The spacious grounds encompass beautiful flower & vegetable gardens & mature trees.</p>
+			<a href="#" class="btn">Book Your Event</a>
+		</div>
+	</section>
+</article>
+<?php
+get_footer();
