7fa27cf5093d345815349ba5940f80e3f2023b97
Travis Wagner <travis@travisaw.com>
2021-02-18 23:13:28 -0500

    the_content

--- wp-content/themes/sherwood/page.php
+++ wp-content/themes/sherwood/page.php
@@ -8,22 +8,7 @@
 get_header();
 ?>
 <article id="content" class="content" role="content">
-
-	<div class="container mw-md py-5">
-	<?php
-	if ( have_posts() ) :
-		while ( have_posts() ) :
-			?>
-			<?php
-			the_post();
-			the_content();
-			?>
-			<?php
-		endwhile;
-	endif;
-	?>
-	</div>
-
+	<?php the_content(); ?>
 </article>
 <?php
 get_footer();
