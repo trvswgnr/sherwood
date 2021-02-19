5cb8f944e49f077743808711841096e524c6fc7f
Travis Wagner <travis@travisaw.com>
2021-02-19 16:28:49 -0500

    add footer

--- wp-content/themes/sherwood/footer.php
+++ wp-content/themes/sherwood/footer.php
@@ -6,6 +6,12 @@
  */
 
 ?>
+<footer class="dark">
+	<div class="container">
+		<p>Copyright © <?php echo date('Y'); ?> Sherwood Gardens</p>
+		<p>Website by <a href="#">TAW Digital</a></p>
+	</div>
+</footer>
 </main><!-- #main -->
 <?php wp_footer(); ?>
 </body>
