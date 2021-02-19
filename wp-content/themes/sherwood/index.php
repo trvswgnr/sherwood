c251dfb85a75c6d47ba986cfc879545401206ecd
Travis Wagner <travis@travisaw.com>
2021-02-18 23:12:29 -0500

    rm container

--- wp-content/themes/sherwood/index.php
+++ wp-content/themes/sherwood/index.php
@@ -7,19 +7,19 @@
 
 get_header();
 ?>
-<article id="content" class="content container-fluid" role="content">
-<?php
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		?>
-		<?php
-		the_post();
-		the_content();
-		?>
-		<?php
-	endwhile;
-endif;
-?>
+<article id="content" class="content" role="content">
+	<?php
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			?>
+			<?php
+			the_post();
+			the_content();
+			?>
+			<?php
+		endwhile;
+	endif;
+	?>
 </article>
 <?php
 get_footer();
