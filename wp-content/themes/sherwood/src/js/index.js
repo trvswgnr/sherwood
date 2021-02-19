f8c64535f7f05ece11bd47c7051aae7f8dec095b
Travis Wagner <travis@travisaw.com>
2021-02-19 16:30:05 -0500

    add vue components, js

--- wp-content/themes/sherwood/src/js/index.js
+++ wp-content/themes/sherwood/src/js/index.js
@@ -6,3 +6,24 @@
 
 // import "regenerator-runtime/runtime";
 import u from './utilities';
+import Vue from 'vue';
+
+// vue components.
+import navigation from './components/navigation.vue';
+import background from './components/background.vue';
+
+new Vue({
+	el: '#main',
+	components: {
+		navigation,
+		background
+	}
+});
+
+
+const vh = window.innerHeight * 0.01;
+const nav = document.getElementById('primaryNav');
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--nav-height', nav.offsetHeight + 'px');
+
+addEventListener('scroll', e => nav.classList[pageYOffset > nav.offsetHeight ? 'add' : 'remove']('scrolled'));
