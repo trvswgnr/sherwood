1bc3ab58effa2626cbd12629e03ca59fa9fafc4c
Travis Wagner <travis@travisaw.com>
2021-03-12 14:01:41 -0500

    minor

--- wp-content/themes/sherwood/front-page.php
+++ wp-content/themes/sherwood/front-page.php
@@ -12,7 +12,7 @@ get_header();
 		<background src="<?php echo get_cdn_url('behind-house.png'); ?>"></background>
 		<div class="container container-md">
 			<h1>A Place to Gather</h1>
-			<p>Sherwood Gardens is situated on 10 acres and is eagerly awaiting your next wedding, gathering or celebration. The 1903 historic home is fully equipped with everything you would need for your stay including 5 bedrooms, 3 bathrooms, and 2 glassed in sun porches.</p>
+			<p>Sherwood Gardens is situated on 10 acres and is eagerly awaiting your next wedding, gathering or celebration. The 1903 historic home is fully equipped with everything you would need for your stay including 5 bedrooms, 3 bathrooms, and 2 glassed-in sun porches.</p>
 			<p>The Event Barn has a main floor and upstairs for celebrating, and can accommodate up to 150 guests. The spacious grounds encompass beautiful flower and vegetable gardens along with mature trees.</p>
 			<a href="#" class="btn">Book Your Event</a>
 		</div>
