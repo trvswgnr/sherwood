87bd675c2628f4e6ebcc38849ba2f8c1fbf39e07
Travis Wagner <travis@travisaw.com>
2021-03-16 20:21:39 -0400

    update buttons

--- wp-content/themes/sherwood/front-page.php
+++ wp-content/themes/sherwood/front-page.php
@@ -14,7 +14,7 @@ get_header();
 			<h1>A Place to Gather</h1>
 			<p>Sherwood Gardens is situated on 10 acres and is eagerly awaiting your next wedding, gathering or celebration. The 1903 historic home is fully equipped with everything you would need for your stay including 5 bedrooms, 3 bathrooms, and 2 glassed-in sun porches.</p>
 			<p>The Event Barn has a main floor and upstairs for celebrating, and can accommodate up to 150 guests. The spacious grounds encompass beautiful flower and vegetable gardens along with mature trees.</p>
-			<a href="#" class="btn">Book Your Event</a>
+			<a href="<?php echo site_url('/contact'); ?>" class="btn">Book Your Event</a>
 		</div>
 	</section>
 	<section class="section">
@@ -24,7 +24,7 @@ get_header();
 				<figcaption>
 					<h2>Our Vision</h2>
 					<p>We started Sherwood Gardens to create a serene environment for all to enjoy. There's nothing more important to us than making sure your special day is just that—special.</p>
-					<a href="#" class="btn">View Gallery</a>
+					<a href="<?php echo site_url('/accommodations'); ?>" class="btn">View Gallery</a>
 				</figcaption>
 			</figure class="media-content">
 		</div>
@@ -34,7 +34,7 @@ get_header();
 		<div class="container container-md">
 			<h2 class="h1">An Unforgettable Experience</h2>
 			<p>Sherwood Gardens provides an intimate and memorable setting for Weddings, Receptions, Rehearsal dinners, Memorials, Vacations, Slumber parties, Family gatherings, Reunions, & Retreats.</p>
-			<a href="#" class="btn">Book Your Event</a>
+			<a href="<?php echo site_url('/contact'); ?>" class="btn">Request More Info</a>
 		</div>
 	</section>
 	<section class="section">
@@ -43,7 +43,7 @@ get_header();
 				<figcaption>
 					<h2 style="font-size: calc(1rem + 1vw);">Southwest Michigan's Best Kept Secret</h2>
 					<p>Sherwood Gardens is proud to offer a completely renovated 1903 Colonial home and grounds presented in a warm and unique style. The entire estate is available for Weekend Rental.</p>
-					<a href="#" class="btn">See Our Accomodations</a>
+					<a href="<?php echo site_url('/accommodations'); ?>" class="btn">See Our Accomodations</a>
 				</figcaption>
 				<img src="https://02f0a56ef46d93f03c90-22ac5f107621879d5667e0d7ed595bdb.ssl.cf2.rackcdn.com/sites/1632/photos/730141/fall_wedding_2017_21220171109-32253-lscsgf_x435.JPG" alt="Behind House" />
 			</figure class="media-content">
